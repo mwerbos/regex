7de783d755940616800e54d292a79ef6110bf1ba
Maia Werbos <tigrennatenn@yahoo.com>
2015-08-27 23:16:13 -0700

    Cleaned up processing code

--- src/Regex.hs
+++ src/Regex.hs
@@ -8,19 +8,36 @@ matchExpression regex string = runAutomaton automaton string
   where automaton = processRegex regex
 
 runAutomaton :: Automaton -> String -> [Interval]
-runAutomaton automaton string = endIntervals
-  where (endStates, endIntervals) = foldl (runAutomatonOnce automaton) ([S 0],[]) string
+runAutomaton automaton string = currentMatches end_state
+  where end_state = foldl (runAutomatonOnce automaton) initialState string
 
-runAutomatonOnce :: Automaton -> ([State],[Interval]) -> Char -> ([State],[Interval])
-runAutomatonOnce automaton (states, intervals) char = (popped_states, popped_intervals)
+data PossibleMatch = P { state :: State, startIndex :: Int }
+
+data ProcessingState = ProcessingState {
+    possibleMatches :: [PossibleMatch],
+    currentIndex :: Int,
+    currentMatches :: [Interval]
+}
+
+initialMatch :: PossibleMatch
+initialMatch = P { state = S 0, startIndex = 0 }
+
+initialState :: ProcessingState
+initialState = ProcessingState {
+    possibleMatches = [initialMatch],
+    currentIndex = 0,
+    currentMatches = []
+}
+
+runAutomatonOnce :: Automaton -> ProcessingState -> Char -> ProcessingState
+runAutomatonOnce automaton state char = popFinalStates (runStatesOnce automaton state char)
         -- First get new states by running the automaton
-  where new_states = runStatesOnce automaton states char
         -- Then pop any final states onto the intervals list
-        (popped_states,popped_intervals) = popFinalStates new_states intervals
-        
 
-popFinalStates :: [State] -> [Interval] -> ([State], [Interval])
-popFinalStates = error "popFinalStates undefined"
-
-runStatesOnce :: Automaton -> [State] -> Char -> [State]
+-- Runs the automaton over every current possible match, with the given character
+runStatesOnce :: Automaton -> ProcessingState -> Char -> ProcessingState
 runStatesOnce = error "runStatesOnce undefined" 
+
+-- Removes all states that are the final state and turns them into matches
+popFinalStates :: ProcessingState -> ProcessingState
+popFinalStates = error "popFinalStates undefined"
