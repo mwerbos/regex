9cdef09d440c5902d0a6f55f040ec3b259a8b03b
mwerbos <mwerbos@localhost>
2015-10-05 20:15:32 -0700

    modified relabelAndTranslate to return a map from old nodes to new ones

--- src/Regex/Util.hs
+++ src/Regex/Util.hs
@@ -3,6 +3,7 @@ module Regex.Util (addGraphs) where
 import Data.Graph.Inductive.Graph (newNodes,buildGr,ufold,insEdges,insNode,delNode,Context(..),Node,labNodes)
 import Data.Graph.Inductive.PatriciaTree (Gr(..))
 import Data.List (elem,delete)
+import qualified Data.Map.Strict as M
 
 addGraphs :: Gr a b -> Gr a b -> Gr a b
 addGraphs one two = smoosh one (relabel one two)
@@ -29,13 +30,13 @@ relabel one two = fst $ ufold relabelNode (two,next_nodes) two
 
 -- Takes a list of nodes-of-interest from the second graph,
 -- and returns their new indices in the resulting graph.
-relabelAndTranslate :: Gr a b -> (Gr a b,[Node]) -> (Gr a b,[Node])
+relabelAndTranslate :: Gr a b -> (Gr a b,[Node]) -> (Gr a b,M.Map Node Node)
 relabelAndTranslate one (two,interesting_nodes) =
   (relabeled_graph, relabeled_nodes)
   -- TODO: Figure out how to note down whenever one of the nodes of interest is modified,
   -- and put its new index in the updated list of interesting nodes.
-  where relabelNode :: Context a b -> (Gr a b, [Node], ([Node],[Node]))
-            -> (Gr a b, [Node], ([Node],[Node]))
+  where relabelNode :: Context a b -> (Gr a b, [Node], ([Node],M.Map Node Node))
+            -> (Gr a b, [Node], ([Node],M.Map Node Node))
         relabelNode (in_edges, node, thing, out_edges)
           (graph,(next_node:rest_nodes), (old_nodes,new_nodes)) =
             (insEdges (reformat_edges next_node in_edges) $
@@ -44,12 +45,12 @@ relabelAndTranslate one (two,interesting_nodes) =
              insNode (next_node, thing) graph,
              rest_nodes,
              try_relabel_nodes node next_node (old_nodes,new_nodes))
-        try_relabel_nodes :: Node -> Node -> ([Node],[Node]) -> ([Node],[Node])
-        try_relabel_nodes node next_node (old_nodes,new_nodes) = 
+        try_relabel_nodes :: Node -> Node -> ([Node],M.Map Node Node) -> ([Node],M.Map Node Node)
+        try_relabel_nodes node next_node (old_nodes,new_node_map) = 
             if node `elem` old_nodes
-            then (delete node old_nodes, next_node:new_nodes)
-            else (old_nodes,new_nodes)
+            then (delete node old_nodes, M.insert node next_node new_node_map)
+            else (old_nodes,new_node_map)
         reformat_edges next_node = map (\(edge_thing, other_node) -> (other_node, next_node, edge_thing))
         next_nodes = newNodes (length $ labNodes two) one
         (relabeled_graph, _, (_,relabeled_nodes)) =
-            ufold relabelNode (two,next_nodes,(interesting_nodes,[])) two 
+            ufold relabelNode (two,next_nodes,(interesting_nodes,M.empty)) two 
