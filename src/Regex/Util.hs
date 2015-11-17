4b7568ce757aafa6a0a1a66a81dd70dc416ef1ab
Maia Werbos <tigrennatenn@yahoo.com>
2015-11-16 19:54:11 -0800

    Rewrote relabelNode function to be more comprehensible. I now comprehend more of its brokenness.

--- src/Regex/Util.hs
+++ src/Regex/Util.hs
@@ -29,31 +29,62 @@ relabel one two = fst $ ufold relabelNode (two,next_nodes) two
         reformat_edges next_node = map (\(edge_thing, other_node) -> (other_node, next_node, edge_thing))
         next_nodes = newNodes (length $ labNodes two) one
 
+-- Relabels the second graph so it's compatible with the first.
+-- Also:
 -- Takes a list of nodes-of-interest from the second graph,
 -- and returns their new indices in the resulting graph.
 relabelAndTranslate :: (Show a, Show b) => Gr a b -> (Gr a b,[Node]) -> (Gr a b,M.Map Node Node)
-relabelAndTranslate one (two,interesting_nodes) =
+relabelAndTranslate base_graph (graph_to_relabel, interesting_nodes) =
   (relabeled_graph, relabeled_nodes)
-  where (relabeled_graph, _, (_,relabeled_nodes)) =
-            ufold relabelNode (two,next_nodes,(interesting_nodes,M.empty)) two 
-        next_nodes = newNodes (length $ labNodes two) one
+  where RelabelContext { partlyRelabeledGraph = relabeled_graph, translatedNodes = relabeled_nodes } =
+            ufold relabelNode initial_context graph_to_relabel
+        initial_context = RelabelContext {
+                               partlyRelabeledGraph = graph_to_relabel,
+                               newNodeLabels = next_nodes_for_base_graph,
+                               remainingNodesToTranslate = interesting_nodes,
+                               translatedNodes = M.empty }
+        next_nodes_for_base_graph = newNodes (length $ labNodes graph_to_relabel) base_graph
 
--- Factored out for... reasons
-relabelNode :: (Show a, Show b) => Context a b -> (Gr a b, [Node], ([Node],M.Map Node Node))
-    -> (Gr a b, [Node], ([Node],M.Map Node Node))
-relabelNode (in_edges, node, thing, out_edges)
-  (graph,(next_node:rest_nodes), (old_nodes,new_nodes)) =
-    trace ("relabeling node " ++ show node ++ " in graph " ++ show graph) $
-    (insEdges (reformat_edges next_node in_edges) $
-     insEdges (reformat_edges next_node out_edges) $
-     delNode node $
-     insNode (next_node, thing) graph,
-     rest_nodes,
-     try_relabel_nodes node next_node (old_nodes,new_nodes))
-  where try_relabel_nodes :: Node -> Node -> ([Node],M.Map Node Node) -> ([Node],M.Map Node Node)
+data RelabelContext a b = RelabelContext {
+  partlyRelabeledGraph :: Gr a b,
+  newNodeLabels :: [Node],
+  remainingNodesToTranslate :: [Node],
+  translatedNodes :: M.Map Node Node
+}
+
+relabelNode :: (Show a, Show b) => Context a b -> RelabelContext a b -> RelabelContext a b
+relabelNode (in_edges_to_relabel, node_to_relabel, node_payload, out_edges_to_relabel)
+  old_relabel_context = -- (graph_to_relabel, (next_node:rest_nodes), (old_nodes,new_nodes)) =
+    trace ("relabeling node " ++ show node_to_relabel ++
+           " in graph " ++ show (partlyRelabeledGraph old_relabel_context)) $
+    RelabelContext {
+      partlyRelabeledGraph =
+        insEdges (reformat_edges next_available_node in_edges_to_relabel) $
+        insEdges (reformat_edges next_available_node out_edges_to_relabel) $
+        delNode node_to_relabel $
+        insNode (next_available_node, node_payload) $ partlyRelabeledGraph old_relabel_context,
+
+      -- By construction of the fold, this should never crash.
+      newNodeLabels = new_available_nodes,
+      -- By construction of the fold, this should never crash.
+      remainingNodesToTranslate = tail $ remainingNodesToTranslate old_relabel_context,
+
+      translatedNodes = translated_nodes
+    }
+  where (new_available_nodes, translated_nodes) =
+          try_relabel_nodes node_to_relabel
+                            next_available_node
+                            (remainingNodesToTranslate old_relabel_context,
+                            translatedNodes old_relabel_context)
+
+        try_relabel_nodes :: Node -> Node -> ([Node],M.Map Node Node) -> ([Node],M.Map Node Node)
         try_relabel_nodes node next_node (old_nodes,new_node_map) = 
             if node `elem` old_nodes
             then (delete node old_nodes, M.insert node next_node new_node_map)
             else (old_nodes,new_node_map)
 
-        reformat_edges next_node = map (\(edge_thing, other_node) -> (other_node, next_node, edge_thing))
+        -- By construction of the fold, this should never crash.
+        next_available_node = head $ newNodeLabels old_relabel_context
+
+        reformat_edges next_available_node =
+            map (\(edge_payload, other_node_in_edge) -> (other_node_in_edge, next_available_node, edge_payload))
