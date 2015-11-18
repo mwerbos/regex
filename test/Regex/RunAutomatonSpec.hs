4b7db94e0280581cd05d986a3e9e52e8e4d19642
mwerbos <mwerbos@localhost>
2015-11-17 22:48:21 -0800

    Added test for popFinalStates and more runAutomaton tests. Turns out it works fine if it sees a match starting at 0! I think I might know what this is.

--- test/Regex/RunAutomatonSpec.hs
+++ test/Regex/RunAutomatonSpec.hs
@@ -18,6 +18,10 @@ spec = do
   describe "runAutomaton" $ do
     it "runs a small automaton on a short string" $ do
       runAutomaton simple_automaton "the fox was " `shouldBe` [Interval (4, 7)]
+    it "runs the automaton on the minimal string" $ do
+      runAutomaton simple_automaton "fox" `shouldBe` [Interval (0, 3)]
+    it "runs the automaton on the a slightly less minimal string" $ do
+      runAutomaton simple_automaton " fox" `shouldBe` [Interval (1, 4)]
 
   describe "runAutomatonOnce" $ do
     it "moves to the next state on seeing the right character" $ do
@@ -26,6 +30,18 @@ spec = do
             possibleMatches = [P {matchState = 1, startIndex = 0 }],
             currentIndex = 1,
             currentMatches = []
+          }
+    it "moves to the end state on seeing the right character" $ do
+      let almostEndState = ProcessingState {
+            possibleMatches = [P {matchState = 4, startIndex = 0 }],
+            currentIndex = 1,
+            currentMatches = []
+          }
+      runAutomatonOnce simple_automaton almostEndState 'x' `shouldBe`
+          ProcessingState {
+            possibleMatches = [],
+            currentIndex = 2,
+            currentMatches = [Interval (0, 2)]
           }
     it "does not move on after seeing a wrong character" $ do
       runAutomatonOnce simple_automaton initialState 'r' `shouldBe`
@@ -54,6 +70,20 @@ spec = do
     it "does not move on after seeing a wrong character" $ do
       runNonEpsilonMoves simple_automaton initialState 'r' `shouldBe`
           initialState { possibleMatches = [] }
+
+  describe "popFinalStates" $ do
+    it "grabs a single final state" $ do
+      let state_with_unpopped_match = ProcessingState {
+            possibleMatches = [P {matchState = 2, startIndex = 0}],
+            currentIndex = 2,
+            currentMatches = []
+          }
+          state_with_popped_match = ProcessingState {
+            possibleMatches = [],
+            currentIndex = 2,
+            currentMatches = [Interval (0, 2)]
+          } 
+      popFinalStates 2 state_with_unpopped_match `shouldBe` state_with_popped_match
 
 main :: IO ()
 main = hspec spec
