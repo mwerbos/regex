64cce505e3a0f401064da14e7deb6dda58301cb5
mwerbos <mwerbos@localhost>
2015-11-19 17:26:35 -0800

    Defined matching on Wildcard token

--- src/Regex/Match.hs
+++ src/Regex/Match.hs
@@ -5,4 +5,5 @@ import Regex.Data
 matchesToken :: Char -> Token -> Bool
 matchesToken c (Single token_char) = c == token_char
 matchesToken c (NegChar token_char) = c /= token_char
+matchesToken c Wildcard = True
 matchesToken _ t = error ("matchesToken undefined for this token: " ++ show t)
